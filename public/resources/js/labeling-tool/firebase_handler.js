7626796614677189fdc0d1e4bb738a345984f4b3
DoubleBite <r05921030@ntu.edu.tw>
2018-06-05 16:47:42 +0800

    First commit

---
+++ public/resources/js/labeling-tool/firebase_handler.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDniuCMGZNMlwOOct4o9xb5I18MizkI1EE",
+    authDomain: "dddddd-55995.firebaseapp.com",
+    databaseURL: "https://dddddd-55995.firebaseio.com",
+    projectId: "dddddd-55995",
+    storageBucket: "dddddd-55995.appspot.com",
+    messagingSenderId: "704147310036"
+};
+firebase.initializeApp(config);
